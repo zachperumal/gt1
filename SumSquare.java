ea4f7e079803473b86204d61233b9aefac1d0ee7
avu6 <48440153+avu6@users.noreply.github.com>
2019-03-13 08:22:20 -0400

    Update SumSquare.java

--- SumSquare.java
+++ SumSquare.java
@@ -3,7 +3,10 @@ public class SumSquare {
     java.util.Scanner input = new java.util.Scanner(System.in);
     System.out.println("Your program will generate an output of Sum of Squares of multiples of 3"
                        + "\nfrom 3 to 33.");
-  
+  int num = 3;
+    System.out.println("Cummulation: " + (num * num) + " : " + num + " * " + num);
+    num = num +3;
+    
     
   
   
